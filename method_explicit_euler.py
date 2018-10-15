d8532502edc12a00a9dbf1bff304e73fa22faaec
Christoph Hoeppke <christoph.hoeppke@maths.ox.ac.uk>
2018-10-15 16:14:09 +0100

    Added base files
    Signed-off-by: Christoph Hoeppke <christoph.hoeppke@maths.ox.ac.uk>

---
+++ method_explicit_euler.py
@@ -0,0 +1,12 @@
+from step_method import step_method
+
+
+class explicit_euler(step_method):
+
+    """Docstring for explicit_euler. """
+
+    def __init__(self):
+        """TODO: to be defined1. """
+        step_method.__init__(self)
+
+
---
+++ step_method.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+class step_method(object):
+
+    """Docstring for step_method.
+        Time stepping method for ODE's
+    """
+
+    def __init__(self):
+        """TODO: to be defined1. """
+
+    def newton_iteration():
+        pass
