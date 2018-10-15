a615bdd0811982e7d59184ed43b6bbf4c92e566a
iapuiu <ioan.puiu@maths.ox.ac.uk>
2018-10-16 00:03:30 +0100

    added the test for MPR, I get an error with y=sparse.linalg.solve(A,b) line, I just can't see why

--- method_midpointrule.py
+++ method_midpointrule.py
@@ -1,21 +1,26 @@
 from step_method import StepMethod
+from rhs_function import RHSFunction
+from scipy import optimize
 import scipy.sparse as sparse
+import numpy as np
 
 class MidPointRule(StepMethod):
 
-    """
+    """ 
         This implements MPR
     """
 
-    def step(self,func,uvec,time,steplen,tolerance=10**(-8))
+    def step(self, func, uvec, time, steplen, tolerance=10**(-8)):
         y=np.copy(uvec)
         yprev=np.copy(uvec)
         err=1
         numit=0
-        while err > tolerance and numit < 5:
+        I=sparse.eye(len(uvec))
+        while err>tolerance and numit<5:
             yprev=np.copy(y)
-            A=(np.eye(len(uvec))-steplen/2.0*func.jacobian(yprev,time))
-            F=yprev-uvec-steplen/2*(func.eval(yprev,time)+funct.eval(uvec,time))
-            b=-F+np.dot(A,yprev)
+            A=(I-steplen/2.0*func.jacobian(yprev,time))
+            F=yprev-uvec-steplen/2*(func.eval(yprev,time)+func.eval(uvec,time))
+            b=-np.dot(I,F)+np.dot(A,yprev)
             y=sparse.linalg.spsolve(A,b)
+            numit += 1
         return y
---
+++ method_midpointrule_test.py
@@ -0,0 +1,30 @@
+from method_midpointrule import MidPointRule
+from rhs_function import ExampleFunc01
+from rhs_function import ExampleFunc01_solution
+import matplotlib.pyplot as plt
+import numpy as np
+import unittest
+
+class TestMidPointRule(unittest.TestCase):
+    
+    def test_accuracy01(self):
+        N=10**3
+        t=np.linspace(0,1,num=N)
+        y0=np.array([np.pi]) 
+        exactSol=ExampleFunc01_solution(y0,t).T
+
+        #  Compute numerical sol:
+        mpr_solver = MidPointRule(N,y0,[0,1], ExampleFunc01())
+        solution = mpr_solver.generate()
+        numericSol = np.zeros_like(exactSol)
+        idx = 0
+        for (time, val) in solution:
+            numericSol[idx]=val
+            idx+=1
+
+        err=np.max(np.abs(exactSol-numericSol))
+        print(err)
+        self.assertTrue(err<4.0*10**(-7))
+
+if __name__== "__main__":
+    unittest.main()
