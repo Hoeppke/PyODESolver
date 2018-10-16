26f060a53cbcd029a2980b3880a1f0f1d657c60d
Christoph Hoeppke <christoph.hoeppke@maths.ox.ac.uk>
2018-10-16 14:46:13 +0100

    updates to the midpoint rule and mpr-test
    Signed-off-by: Christoph Hoeppke <christoph.hoeppke@maths.ox.ac.uk>

--- method_implicit_euler.py
+++ method_implicit_euler.py
@@ -13,7 +13,7 @@ class ImplicitEuler(StepMethod):
     def __init__(self, N, y0, domain, func):
         StepMethod.__init__(self, N, y0, domain, func)
 
-    def step(self, f, u, t, h, tol=10**(-8), maxiter=100):
+    def step(self, f, u, t, h, tol=10**(-10), maxiter=10):
         t_new = t + h
         t_old = t
         # Create a copy of u
@@ -24,7 +24,8 @@ class ImplicitEuler(StepMethod):
 
         # Compute the function required for Newton iteration.
         def myF(y_new):
-            val = y_new - y_old - 0.5 * h * (f.eval(y_new, t_new) + f.eval(y_old, t_old))
+            val = y_new - y_old
+            val = val - 0.5 * h * (f.eval(y_new, t_new) + f.eval(y_old, t_old))
             return val
 
         def myJacF(y_new):
@@ -34,7 +35,7 @@ class ImplicitEuler(StepMethod):
 
         itercount = 0
         err = 1
-        while err > tol and itercount < 5:
+        while err > tol and itercount < maxiter:
             Jac = myJacF(y_new)
             Fval = myF(y_new)
             y_update = sparse.linalg.spsolve(Jac, Fval)
--- method_midpointrule.py
+++ method_midpointrule.py
@@ -4,24 +4,37 @@ from scipy import optimize
 import scipy.sparse as sparse
 import numpy as np
 
+
 class MidPointRule(StepMethod):
 
-    """ 
+    """
         This implements MPR
     """
 
-    def step(self, func, uvec, time, steplen, tolerance=10**(-8)):
-        y=np.copy(uvec)
-        yprev=np.copy(uvec)
-        err=1
-        numit=0
-        I=sparse.eye(len(uvec))
-        while err>tolerance and numit<5:
-            yprev=np.copy(y)
-            A=(I-steplen/2.0*func.jacobian(yprev,time))
-            F=yprev-uvec-steplen/2*(func.eval(yprev,time)+func.eval(uvec,time))
-            b=-np.dot(I,F)+np.dot(A,yprev)
-            y=sparse.linalg.spsolve(A,b)
+    def step(self, func, uvec, time, steplen, tolerance=10**(-10), maxiter=10):
+        t_old = time
+        t_new = time + steplen
+        y_old = np.copy(uvec)
+        y_new = np.copy(uvec) + (steplen/2) * func.eval(y_old, t_old)
+        err = 1
+        numit = 0
+        sp_I = sparse.eye(len(uvec))
+        func_old = func.eval(y_old, t_old)
+
+        def myF(y_new):
+            val1 = (func.eval(y_new, t_new) + func_old)
+            val2 = y_new - y_old - (steplen/2) * val1
+            return val2
+
+        def myJacF(y_new):
+            A = (sp_I  - steplen/2.0 * func.jacobian(y_new, y_new))
+            return A
+
+        while err > tolerance and numit < maxiter:
+            Jac = myJacF(y_new)
+            Fval = myF(y_new)
+            y_update = sparse.linalg.spsolve(Jac, Fval)
+            y_new = y_new - y_update
             numit += 1
-            err=np.max(np.abs(F))
-        return y
+            err = np.max(np.abs(myF(y_new)))
+        return y_new
