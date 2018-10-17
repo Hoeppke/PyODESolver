e9168c5887b9be06a0f6d3fc002f86ba4376c258
Christoph Hoeppke <christoph.hoeppke@maths.ox.ac.uk>
2018-10-17 17:31:19 +0100

    formatting
    Signed-off-by: Christoph Hoeppke <christoph.hoeppke@maths.ox.ac.uk>

--- method_RK4_test.py
+++ method_RK4_test.py
@@ -8,21 +8,21 @@ from rhs_function import ExampleFunc01_solution
 class TestRK4(unittest.TestCase):
 
         def test_accuracy01(self):
-            N=10**3
-            t= np.linspace(0,1,num=N)
-            y0=np.array([np.pi])
-            exactSol=ExampleFunc01_solution(y0,t).T
+            N = 10**3
+            t = np.linspace(0, 1, num=N)
+            y0 = np.array([np.pi])
+            exactSol = ExampleFunc01_solution(y0, t).T
 
             #  Compute numerical solution:
-            RK4_solver=RK4(N,y0,[0, 1], ExampleFunc01())
-            solution=RK4_solver.generate()
-            numericSol=np.zeros_like(exactSol)
+            RK4_solver = RK4(N, y0, [0, 1], ExampleFunc01())
+            solution = RK4_solver.generate()
+            numericSol = np.zeros_like(exactSol)
             idx = 0
-            for (time,val) in solution:
+            for (time, val) in solution:
                 numericSol[idx] = val
-                idx+=1
+                idx += 1
 
-            err=np.max(np.abs(exactSol-numericSol))
+            err = np.max(np.abs(exactSol-numericSol))
             print(err)
             self.assertTrue(err < 4.0*10**(-7))
 
