dcf6060e32a913fcdfce78564e74374adeccc095
Christoph Hoeppke <christoph.hoeppke@maths.ox.ac.uk>
2018-10-18 20:52:34 +0100

    added all files to the new structure
    Signed-off-by: Christoph Hoeppke <christoph.hoeppke@maths.ox.ac.uk>

---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+setup(
+    name='pyodesolver',    # This is the name of your PyPI-package.
+    version='0.1',                          # Update the version number for new releases
+    scripts=['helloworld']                  # The name of your scipt, and also the command you'll be using for calling it
+)
